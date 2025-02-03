d6a41528d2f0e52c16a0166d692b3ff0f0e2e810
jeanLeal <jeanlds13@gmail.com>
2025-02-03 10:50:36 -0300

    finalizando o cadastro das despesas e receitas

--- app/pages/New/index.js
+++ app/pages/New/index.js
@@ -1,15 +1,63 @@
 import React, { useState }from "react";
 import { Background, Input, SubmitButton, SubmitText } from './styles';
 
-import { SafeAreaView, TouchableWithoutFeedback, Keyboard} from "react-native";
+import { SafeAreaView, TouchableWithoutFeedback, Keyboard, Alert} from "react-native";
+import { useNavigation } from '@react-navigation/native';
 
 import Header from "../../components/Header";
 import RegisterTypes from "../../components/RegisterTypes";
 
+import api from "../../services/api";
+
+import { format } from "date-fns";
+
 export default function New() {
+  const navigation = useNavigation();
+  
   const [labelInput, setLabelInput] = useState('');
   const [valueInput, setValueInput] = useState('');
   const [type, setType] = useState('receita');
+
+  function handleSubmit(){
+    Keyboard.dismiss();
+    if (isNaN(parseFloat(valueInput)) || type === null) {
+      alert("Preencha todos os campos");
+      return;
+    }
+
+    Alert.alert(
+      'Confirmando dados', 
+      `Tipo: ${type} - Valor: ${parseFloat(valueInput)}`, 
+      [
+        {
+          text:  'Cancelar',
+          style: 'cancel'
+        },
+        {
+          text: 'Continuar', 
+          style: 'default',
+          onPress: () => handleAdd()
+        }
+      ]
+    )
+  }
+
+  async function handleAdd (){
+    Keyboard.dismiss();
+
+    await api.post('/receive', {
+      description: labelInput,
+      value: Number(valueInput),
+      type: type,
+      date: format(new Date(), 'dd/MM/yyy')
+    }).catch(
+     ()=> alert("Erro ao efetuar o cadastro.") 
+    )
+
+    setLabelInput('');
+    setValueInput('');
+    navigation.navigate('Home')
+  }
 
   return (
     <TouchableWithoutFeedback onPress={()=> Keyboard.dismiss()}>
@@ -31,7 +79,7 @@ export default function New() {
 
           <RegisterTypes type={type} sendTypeChanged={(item)=> setType(item)}/>
 
-          <SubmitButton>
+          <SubmitButton onPress={handleSubmit}>
             <SubmitText>Registrar</SubmitText>
           </SubmitButton>
         </SafeAreaView>
